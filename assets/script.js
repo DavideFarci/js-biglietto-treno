c965b06ccf33edd2773a1fe7a9629589b9a2b87e
DavideFarci <yank92.df@gmail.com>
2023-04-05 18:01:33 +0200

    move calc in if

--- assets/script.js
+++ assets/script.js
@@ -11,21 +11,19 @@ const age = parseInt(prompt("Quanti anni hai?"));
 let KMprice = numKM * 0.21 ;
 let finalKMprice = KMprice.toFixed(2);
 
-let prezzoMinore = KMprice - (KMprice * 20 / 100);
-let finalPriceM = prezzoMinore.toFixed(2);
-
-let prezzoOver = KMprice - (KMprice * 40 / 100);
-let finalPriceO = prezzoOver.toFixed(2);
-
 
 
 if (age > 18 && age < 65) {
     document.getElementById("price").innerHTML = KMprice
 }
 if (age < 18) {
+    let prezzoMinore = KMprice - (KMprice * 20 / 100);
+    let finalPriceM = prezzoMinore.toFixed(2);
     document.getElementById("price").innerHTML = finalPriceM;
 }
 if (age > 65) {
+    let prezzoOver = KMprice - (KMprice * 40 / 100);
+    let finalPriceO = prezzoOver.toFixed(2);
     document.getElementById("price").innerHTML = finalPriceO;
 }
 
